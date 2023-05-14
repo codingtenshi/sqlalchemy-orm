d798eab1c68b73f526aa0c2dc5dd381ef7431338
kireitensh <karina.gornicka@wp.pl>
2023-05-14 12:28:30 +0200

    refactoring

--- models.py
+++ models.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Column, Integer, String, create_engine
+import datetime
 
+from sqlalchemy import Column, Integer, String, create_engine, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
     "mysql+pymysql://root:root@localhost:3306/company"
 
 )
+Session = sessionmaker(bind=engine)
 Base = declarative_base(bind=engine)
+
+
+def main():
+    session = Session()
 
 
 class User(Base):
@@ -16,6 +23,12 @@ class User(Base):
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
+
+    creation_date = Column(
+        DateTime,
+        default=datetime.datetime.now,
+        nullable=False
+    )
 
     def __repr__(self):
         return f"<User {self.first_name} {self.last_name}>"
